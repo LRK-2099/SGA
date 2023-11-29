72a437f7c0058422eef2f682ecb7f4837bbf5e7d
LRK-2099 <143131113+LRK-2099@users.noreply.github.com>
2023-11-29 12:54:53 -0500

    confirmation prisma is operational

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,9 +1,11 @@
 const prisma = require("../prisma");
 
-//This line imports the Prisma client, 
+//This line imports the Prisma client,
 //which is an auto-generated query builder for interacting with the database.
 
 /** Seeds the database with a user and some tasks */
+
+// we have 15 students 
 const seed = async () => {
   await prisma.senator.create({
     data: {
@@ -13,16 +15,124 @@ const seed = async () => {
     },
   });
 };
- await prisma.senator.create ({
-data: {
-  firstName: "",
-  lastName: "",
-  email: "",
-  imageUrl: "",
-  major:""
-}
- })
- await prisma.senator.create ({
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create({
+  data: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    imageUrl: "",
+    major: "",
+  },
+});
+await prisma.senator.create ({
   data: {
     firstName: "",
     lastName: "",
@@ -31,42 +141,6 @@ data: {
     major:""
   }
    })
-   await prisma.senator.create ({
-    data: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      imageUrl: "",
-      major:""
-    }
-     })
-     await prisma.senator.create ({
-      data: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        imageUrl: "",
-        major:""
-      }
-       })
-       await prisma.senator.create ({
-        data: {
-          firstName: "",
-          lastName: "",
-          email: "",
-          imageUrl: "",
-          major:""
-        }
-         })
-         await prisma.senator.create ({
-          data: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            imageUrl: "",
-            major:""
-          }
-           })
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (err) => {
